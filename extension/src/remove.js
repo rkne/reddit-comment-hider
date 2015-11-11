7a8e2391c22886cadffa588b17dc76a5da4c98e7
Andrew Lee <alee@rivierapartners.com>
2015-11-11 11:02:54 -0800

    not even op is safe

--- extension/src/remove.js
+++ extension/src/remove.js
@@ -2,9 +2,8 @@
 
 let bannedStrings = ['my top', 'my highest', 'top comment', 'highest rated', 'gold', 'downvote', 'highest voted',
     'kind stranger', 'my first', 'my inbox', 'blew up', 'overnight', 'front page', 'obligatory', 'gilded']
-let $comments = $('.commentarea .usertext-body > .md > p')
 
-$comments.each((idx, val) => {
+let findAndHide = (idx, val) => {
     let text = val.innerText.toLowerCase()
     if (text.substr(0, 4) === 'edit') {
         if (_.find(bannedStrings, (str) => {
@@ -13,4 +12,10 @@ $comments.each((idx, val) => {
             $(val).hide()
         }
     }
-})
+}
+
+let $comments = $('.commentarea .usertext-body > .md > p')
+$comments.each(findAndHide)
+
+let $op = $('.entry .expando .md > p')
+$op.each(findAndHide)
